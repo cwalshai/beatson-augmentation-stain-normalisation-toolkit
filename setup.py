843a612cd1c54d8b793bce01954ad378effd6e3c
Chris Walsh <4505563+cwalshai@users.noreply.github.com>
2024-01-04 23:38:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install-requires=[
+    install_requires=[
         imagecodecs,
         numpy,
         opencv-python-headless,
